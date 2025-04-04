d054b30a0c5c9d1606f4ccf959f60e3bc673fe3c
Your Name01~git <saifsahai2002@gmail.com>
2025-04-04 11:34:24 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5000
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express()
+require('dotenv').config()
+
+const corsOptions = {
+    origin: ['http://localhost:5173'],
+    credentials: true,
+    optionSuccessStatus: 200
+}
+
+app.use(cors(corsOptions))
+app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.usv0l7z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+    }
+    finally { }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Gamerskit - Backend')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
